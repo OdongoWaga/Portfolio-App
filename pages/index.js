e7705ff0e8de873c0c72b09fc850b980356d827a
JFJ <jfj@JFJs-MacBook-Air.local>
2019-05-10 20:23:04 +0300

    links

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 const Index = () => {
     return (
+        <div>
+
         <h1> I am the Index Page </h1>
+        <a href="/"> Home</a>
+        <a href="/about">About</a>
+        <a href="/portfolios">Portfolios</a>
+        <a href="/blogs">Blog</a>
+        <a href="/cv">Cv</a>
+        </div>
     )
 }
 
---
+++ pages/portfolios.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class portfolios extends Component {
+  render() {
+    return (
+      <div>
+        I am portfolios page
+      </div>
+    )
+  }
+}
