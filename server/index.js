ee9973fa24776fc6858c42fdc0202c3cc9fc80d1
Waga <wagaodongo@gmail.com>
2019-05-19 20:25:41 +0300

    blog changes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const compression = require('compression');
+const path = require('path');
 const next = require('next');
 const mongoose = require('mongoose');
 const routes = require('../routes');
@@ -16,6 +18,14 @@ const bodyParser = require('body-parser');
 
 const bookRoutes = require('./routes/book');
 const portfolioRoutes = require('./routes/portfolio');
+const blogRoutes = require('./routes/blog');
+
+const robotsOptions = {
+  root: path.join(__dirname, "../static"),
+  headers: {
+    'Content-Type': 'text/plain;charset=UTF-8'
+  }
+}
 
 const secretData = [
   {
@@ -37,10 +47,16 @@ mongoose.connect(config.DB_URI, { useNewUrlParser: true})
 app.prepare()
 .then(() => {
   const server = express();
+  server.use(compression());
   server.use(bodyParser.json());
 
   server.use('/api/v1/books', bookRoutes);
   server.use('/api/v1/portfolios', portfolioRoutes);
+  server.use('/api/v1/blogs', blogRoutes);
+
+  server.get('/robots.txt', (req, res) => {
+    return res.status(200).sendFile('robots.txt', robotsOptions);
+  });
 
   server.get('/api/v1/secret', authService.checkJWT, (req, res) => {
     return res.json(secretData);
@@ -60,9 +76,11 @@ app.prepare()
     }
   });
 
-  server.use(handle).listen(3000, (err) => {
+  const PORT = process.env.PORT || 3000;
+
+  server.use(handle).listen(PORT, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on port ' + PORT)
   })
 })
 .catch((ex) => {
