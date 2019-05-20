e1342afa51108bb24964cc6719c6058c59e3e7ea
Waga <wagaodongo@gmail.com>
2019-05-20 09:58:42 +0300

    react toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import App, { Container } from 'next/app';
+import { ToastContainer } from 'react-toastify';
 
 import auth0 from '../services/auth0';
 
 // Stylings
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/main.scss';
+import 'react-toastify/dist/ReactToastify.css';
+
+const namespace = 'http://localhost:3000'
 
 export default class MyApp extends App {
 
@@ -17,7 +21,8 @@ export default class MyApp extends App {
       pageProps = await Component.getInitialProps(ctx)
     }
 
-    const auth = { user, isAuthenticated: !!user };
+    const isSiteOwner = user && user[namespace + '/role'] === 'siteOwner';
+    const auth = { user, isAuthenticated: !!user, isSiteOwner };
 
     return { pageProps, auth }
   }
@@ -27,6 +32,7 @@ export default class MyApp extends App {
 
     return (
       <Container>
+        <ToastContainer />
         <Component {...pageProps} auth={auth}/>
       </Container>
     )
