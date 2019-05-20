4680b8298e68006a0f74b68f4f43f32bdc9928b0
Waga <wagaodongo@gmail.com>
2019-05-20 20:24:11 +0300

    social media links

--- pages/blogs.js
+++ pages/blogs.js
@@ -86,7 +86,7 @@ class Blogs extends React.Component {
                 <div className="col-lg-8 col-md-10 mx-auto">
                   <ul className="list-inline text-center">
                     <li className="list-inline-item">
-                      <a href="#">
+                      <a href="https://twitter.com/wagaodongo">
                         <span className="fa-stack fa-lg">
                           <i className="fas fa-circle fa-stack-2x"></i>
                           <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
@@ -94,15 +94,7 @@ class Blogs extends React.Component {
                       </a>
                     </li>
                     <li className="list-inline-item">
-                      <a href="#">
-                        <span className="fa-stack fa-lg">
-                          <i className="fas fa-circle fa-stack-2x"></i>
-                          <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                        </span>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#">
+                      <a href="https://github.com/OdongoWaga">
                         <span className="fa-stack fa-lg">
                           <i className="fas fa-circle fa-stack-2x"></i>
                           <i className="fab fa-github fa-stack-1x fa-inverse"></i>
